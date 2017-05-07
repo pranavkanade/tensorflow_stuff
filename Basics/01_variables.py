b804f44173766cbebf517f50de74a684fe31c4e6
farziCoder <k.pranav.apk@gmail.com>
2017-05-07 13:37:06 +0530

    first running model is up for basic variable example

--- Basics/01_variables.py
+++ Basics/01_variables.py
@@ -3,4 +3,7 @@ import tensorflow as tf
 x = tf.constant(35, name='x')
 y = tf.Variable(x+5, name='y')
 
-print(y)
+model = tf.global_variables_initializer()
+with tf.Session() as session:
+    session.run(model)
+    print(session.run(y))

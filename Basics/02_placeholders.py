f12a3b6b179cc452461c5671fee3ccf178286b6a
farziCoder <k.pranav.apk@gmail.com>
2017-05-09 03:02:12 +0530

    elementry use of placeholder is shown in the file 02_placeholders.py

---
+++ Basics/02_placeholders.py
@@ -0,0 +1,20 @@
+"""
+This file follows tutorial at : http://learningtensorflow.com/lesson4/
+should visit : http://danijar.com/
+cs224d
+"""
+# Placeholder : These are more basic structures in Just in time programming paradigm
+#         -> A "placeholder" is simply a variable that will be assigned at a later stage.
+#         -> It allows the creation of operation and build our computation graph without needing data
+#         -> At the run time we then feed the data to these placeholders
+
+
+import tensorflow as tf
+import numpy as np
+x = tf.placeholder(tf.float32, name='x')
+
+y = x * 2
+
+with tf.Session() as session:
+    result = session.run(y, feed_dict={x : [1, 2, 3]})
+	print(result)
